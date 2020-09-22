257610b5a00a8ccb1aa7743cae6595f5d5dbf5d7
Corey Farrell <git@cfware.com>
2020-09-22 08:46:17 -0400

    feat: Initial implementation

---
+++ README.md
@@ -0,0 +1,6 @@
+# @cfware/event-blocker [![NPM Version][npm-image]][npm-url]
+
+Basic methods to block events
+
+[npm-image]: https://img.shields.io/npm/v/@cfware/event-blocker.svg
+[npm-url]: https://npmjs.org/package/@cfware/event-blocker
---
+++ event-blocker.js
@@ -0,0 +1,14 @@
+export const preventDefault = event => event?.preventDefault();
+export const stopPropagation = event => event?.stopPropagation();
+export const stopImmediatePropagation = event => event?.stopImmediatePropagation();
+
+export const blockEvent = event => {
+	preventDefault(event);
+	stopPropagation(event);
+};
+
+export const immediateBlockEvent = event => {
+	preventDefault(event);
+	stopPropagation(event);
+	stopImmediatePropagation(event);
+};
